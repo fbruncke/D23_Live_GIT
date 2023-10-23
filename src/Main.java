d85bf361688df7a9834d3d9a0772838813dac833
Frank <fha@easv.dk>
2023-10-23 11:06:49 +0200

    added domagic to Main

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    /**
+     * Example
+     */
+    public static void doMagic()
+    {
+        System.out.println("Fature branch");
+    }
 }
